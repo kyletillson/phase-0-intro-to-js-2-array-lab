e3cb701602bb332269ef72fa26b10cd3988eb670
Kyle Tillson <kyletillson004@gmail.com>
2022-07-18 21:25:37 -0600

    commit tree 9a7a9ad1e0c23f6e60b25744d197d1704aee8942

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(cat) {
+    cats.push(cat);
+}function destructivelyPrependCat(cat) {
+    cats.unshift(cat);
+}
+function destructivelyRemoveLastCat(cat) {
+    cats.pop(cat);
+}
+function destructivelyRemoveFirstCat(cat) {
+    cats.shift(cat);
+}
+function appendCat(cat) {
+    return [...cats, cat];
+}
+function prependCat(cat) {
+    return [cat, ...cats];
+}
+function removeFirstCat () {
+    return cats.slice(1);
+  }
+  
+  function removeLastCat () {
+    return cats.slice(0, cats.length - 1);
+  }
